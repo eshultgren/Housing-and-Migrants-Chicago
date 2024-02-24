bdfbbcb180504f5463584c13473c9b5032443f95
eshultgren <129089900+eshultgren@users.noreply.github.com>
2024-02-24 14:05:45 -0600

    opioid updates

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Housing-and-Migrants-Chicago
 DP II Final
+
+
+
+
+##Opioid Analysis
+Opioid cases by zip code for 2020-2022 came from IDPH's Data Dashboard:
+
+Encampment data is from HUD as well as supplemental eye-witness data collection: https://www.huduser.gov/portal/sites/default/files/pdf/Chicago-Encampment-Report.pdf
+A major limitation in this analysis is the lack of encampment data. This does not capture the unhoused populations that seek shelter on CTA trains. Additionally, besides those included in the pdf, data was limited by the research team's confirmation of encampment locations and size. This means the only data collected is concentrated to areas where the research teams frequents.
+High density areas were pulled from the Point-in-Time sampling methodology.
+
+While it appears there are a concentration of encampments where rates of overdose are high, we are hesitant to make any claims of correlation due to the aforementioned limitations in data collection.
--- merge and create opioid shape file.R
+++ merge and create opioid shape file.R
@@ -25,7 +25,9 @@ age_zip <- read_csv(file.path(path, "acs_med_age_zip.csv"))
 
 edu_zip <- read_csv(file.path(path, "acs_education_zip.csv"))
 
-pop_22 <- read_csv(file.path(path, "population zip 2022.csv"))
+pop_22 <- read_csv(file.path(path, "population zip_2022.csv"))
+pop_21 <- read_csv(file.path(path, "population_zip_2021.csv"))
+pop_20 <- read_csv(file.path(path, "population_zip_2020.csv"))
 
 data20 <- read_csv(file.path(path, "overdose counts 2020.csv"))
 data21 <- read_csv(file.path(path, "overdose counts 2021.csv"))
@@ -49,9 +51,14 @@ opioid_shape <- merge(opioid_shape, age_zip, by = "zip")
 opioid_shape <- merge(opioid_shape, edu_zip, by = "zip")
 ##add population
 opioid_shape <- merge(opioid_shape, pop_22, by = "zip")
+opioid_shape <- merge(opioid_shape, pop_21, by = "zip")
+opioid_shape <- merge(opioid_shape, pop_20, by = "zip")
+
 
 opioid_shape <- opioid_shape %>% 
-  mutate(overdose_rate_22 = Overdose_Count_by_Zip_2022/pop_10000)
+  mutate(overdose_rate_22 = Overdose_Count_by_Zip_2022/pop_10000_2022,
+         overdose_rate_21 = Overdose_Count_by_Zip_2021/pop_10000_2021,
+         overdose_rate_20 = Overdose_Count_by_Zip_2020/pop_10000_2020)
 
 ###This is the opioid file with all the info in it!###
 
@@ -60,6 +67,14 @@ opioid_shape <- opioid_shape %>%
 st_write(opioid_shape,
          "opioid_shape.shp",
          driver="ESRI Shapefile")
+
+
+#encampment data into shape
+encampment <- read_csv(file.path(path, "encampment data.csv"))
+
+encampment <- as.data.frame(encampment) %>% 
+  st_as_sf(coords=c("Longitude","Latitude"), crs=4326, remove=FALSE)  
+
 
 
 ###mapping opioids by year
@@ -122,12 +137,14 @@ print(opioid_unhoused_2022)
 opioid_rate_2022 <- ggplot() +
   geom_sf(data = zip_chi_shape) +
   geom_sf(data = opioid_shape, aes(fill = overdose_rate_22), color = NA) +
-  geom_sf(data = high_dense_shape_22, fill = "darkgoldenrod2", fill = NA) +
-  labs(title = "Overdose Rate Per 10,000 Ppl (Nonfatal and Fatal) by Zip in 2022",
-       subtitle = "Yellow Indicates High-Density Populations of Unhoused People",
+  geom_sf(data = high_dense_shape_22, fill = "darkgoldenrod2") +
+  labs(title = "Overdose Rate (Nonfatal and Fatal Per 10,000) by Zip in 2022",
+       subtitle = "Yellow are areas w/ high-density unhoused pop. Black dots are encampments.",
        caption = "Data: IL Dept of Public Health, ACS") +
-  scale_fill_distiller(name="Opioid Overdose Count", palette = "BuPu", trans = "reverse", 
+  scale_fill_distiller(name="Opioid Overdose Rate", palette = "BuPu", trans = "reverse", 
                        breaks = pretty_breaks(n = 5)) +
+  geom_point(data=encampment, aes(x=`Longitude`, y=`Latitude`, size=Num_Ppl)) +
+  labs(size = "Size of Encampment") +
   theme_map() +
   theme(
     plot.title = element_text(hjust = 0, size= 13),
@@ -234,15 +251,5 @@ age_opi_plot <- ggdraw() +
 print(age_opi_plot)
 
 
-
-
-
-
-
-
-
-
-##shiny
-#manipulate year in drop down to select which column in opioid dataframe
 
 
