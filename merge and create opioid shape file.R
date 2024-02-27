717e564c3854557647d3987630c6a3941ce2307e
eshultgren <129089900+eshultgren@users.noreply.github.com>
2024-02-26 20:04:35 -0600

    saved unhoused csv

--- merge and create opioid shape file.R
+++ merge and create opioid shape file.R
@@ -84,14 +84,20 @@ unhoused_locations_df <- data.frame(
   Location = c("The Loop, River North", "CTA - Red Line (95th/Dan Ryan)", "CTA - Blue Line (Forest Park)",
                "Near West Side/Medical District", "CTA - Blue Line (Cumberland/Rosemont)", "North Side",
                "CTA - Red Line", "South Side (East of State)", "O'Hare Airport", "South Side (West of State)",
-               "CTA - Red Line (Howard)", "West Town, Kennedy Expressway", "Midway Airport Terminal", "Northwest Side"),
-  Responses = c(170, 125, 118, 112, 71, 58, 54, 38, 33, 29, 13, 13, 12, 2),
+               "CTA - Red Line (Howard)", "West Town, Kennedy Expressway", "Midway Airport Terminal", "Northwest Side",
+               "CTA - Blue Line (Forest Park)", "CTA - Red Line (95th/Dan Ryan)", "CTA - Red Line (Howard)",
+               "Near West Side/Medical District", "The Loop, River North", "North Side", "West Town, Kennedy Expressway", "CTA - Blue Line (Cumberland/Rosemont)",  "CTA - Red Line", "Stevenson Expressway (Archer/Canalport)", "O'Hare Airport"),
+  Responses = c(170, 125, 118, 112, 71, 58, 54, 38, 33, 29, 13, 13, 12, 2, 175, 174, 141, 82, 65, 49, 35, 32, 32, 13, 10),
+  Year = c(2023, 2023, 2023, 2023, 2023, 2023, 2023, 2023, 2023, 2023, 2023, 2023, 2023, 2023,
+           2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022),
   Latitude = c(41.89248568824814, 41.72258849002998, 41.87370675013457, 41.86883204433447, 41.983883465320844, 41.90389519059428,
                41.889900182393696, 41.75042676688248, 41.98037675551297, 41.75042676688248, 42.01889620273381, 41.95930482218085,
-               41.78852626097361, 41.88530874443534),
+               41.78852626097361, 41.88530874443534, 41.87370675013457, 41.72258849002998, 42.01889620273381, 41.86883204433447,
+               41.89248568824814, 41.90389519059428, 41.95930482218085, 41.983883465320844, 41.889900182393696, 41.848050, 41.98037675551297),
   Longitude = c(-87.634044880516, -87.62443385767142, -87.81697954232854, -87.67398565436856, -87.83862460424264, -87.63211612952345,
                 -87.62808780459785, -87.63411546128873, -87.90900606226334, -87.63411546128873, -87.67255656226084, -87.74088846041468,
-                -87.74167007391868, -87.79271259245185)
+                -87.74167007391868, -87.79271259245185, -87.81697954232854, -87.62443385767142, -87.67255656226084, -87.67398565436856,
+                -87.634044880516, -87.63211612952345, -87.74088846041468, -87.83862460424264, -87.62808780459785, -87.63485, -87.90900606226334)
 )
 
 # Convert locations dataset into shapefile format
@@ -287,11 +293,11 @@ print(age_opi_plot)
 ##Areas of Concentrated Unhoused
 ##By Year (2020-2021)
 
-##Plot 2022 RATE
-opioid_rate_2022 <- ggplot() +
+##Plot RATE
+opioid_rate_2020 <- ggplot() +
   geom_sf(data = zip_chi_shape) +
-  geom_sf(data = opioid_shape, aes(fill = overdose_rate_22), color = NA) +
-  labs(title = "Opioid Overdose Rate (Nonfatal and Fatal Per 10,000 Ppl) by Zip in 2022",
+  geom_sf(data = opioid_shape, aes(fill = overdose_rate_20), color = NA) +
+  labs(title = "Opioid Overdose Rate (Nonfatal and Fatal Per 10,000 Ppl) by Zip in 2020",
        subtitle = "Dot Size Correlated W/ High-Density Unhoused Pop.",
        caption = "Data: IL Dept of Public Health, ACS, PIT Survey") +
   scale_fill_viridis(name="Opioid Overdose Rate", option = "magma", trans = "reverse", 
@@ -309,4 +315,9 @@ opioid_rate_2022 <- ggplot() +
     legend.title.align = 0.5
   )
 
+print(opioid_rate_2020)
+
+print(opioid_rate_2021)
+
 print(opioid_rate_2022)
+
--- opioid_shiny.R
+++ opioid_shiny.R
@@ -15,35 +15,13 @@ library(shinythemes)
 
 ##Data Prep
 # unhoused locations per PITS report 
-# Compile unhoused locations dataframe 
-unhoused_locations_df <- data.frame(
-  Location = c("The Loop, River North", "CTA - Red Line (95th/Dan Ryan)", "CTA - Blue Line (Forest Park)",
-               "Near West Side/Medical District", "CTA - Blue Line (Cumberland/Rosemont)", "North Side",
-               "CTA - Red Line", "South Side (East of State)", "O'Hare Airport", "South Side (West of State)",
-               "CTA - Red Line (Howard)", "West Town, Kennedy Expressway", "Midway Airport Terminal", "Northwest Side",
-               "CTA - Blue Line (Forest Park)", "CTA - Red Line (95th/Dan Ryan)", "CTA - Red Line (Howard)",
-               "Near West Side/Medical District", "The Loop, River North", "North Side", "West Town, Kennedy Expressway", "CTA - Blue Line (Cumberland/Rosemont)",  "CTA - Red Line", "Stevenson Expressway (Archer/Canalport)", "O'Hare Airport"),
-  Responses = c(170, 125, 118, 112, 71, 58, 54, 38, 33, 29, 13, 13, 12, 2, 175, 174, 141, 82, 65, 49, 35, 32, 32, 13, 10),
-  Year = c(2023, 2023, 2023, 2023, 2023, 2023, 2023, 2023, 2023, 2023, 2023, 2023, 2023, 2023,
-           2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022),
-  Latitude = c(41.89248568824814, 41.72258849002998, 41.87370675013457, 41.86883204433447, 41.983883465320844, 41.90389519059428,
-               41.889900182393696, 41.75042676688248, 41.98037675551297, 41.75042676688248, 42.01889620273381, 41.95930482218085,
-               41.78852626097361, 41.88530874443534, 41.87370675013457, 41.72258849002998, 42.01889620273381, 41.86883204433447,
-               41.89248568824814, 41.90389519059428, 41.95930482218085, 41.983883465320844, 41.889900182393696, 41.848050, 41.98037675551297),
-  Longitude = c(-87.634044880516, -87.62443385767142, -87.81697954232854, -87.67398565436856, -87.83862460424264, -87.63211612952345,
-                -87.62808780459785, -87.63411546128873, -87.90900606226334, -87.63411546128873, -87.67255656226084, -87.74088846041468,
-                -87.74167007391868, -87.79271259245185, -87.81697954232854, -87.62443385767142, -87.67255656226084, -87.67398565436856,
-                -87.634044880516, -87.63211612952345, -87.74088846041468, -87.83862460424264, -87.62808780459785, -87.63485, -87.90900606226334)
-)
+path <- ("C:\\Users\\emmas\\OneDrive\\Documents\\GitHub\\Housing-and-Migrants-Chicago\\outside_data\\")
 
+unhoused_locations_df <- read_csv(file.path(path, "unhoused_locations.csv"))
 
-
-# Convert locations dataset into shapefile format
+# write.csv# Convert locations dataset into shapefile format
 unhoused_locations <- st_as_sf(unhoused_locations_df,
                                coords = c("Longitude", "Latitude"),  crs=4326, remove = FALSE)
-
-path <- "C:\\Users\\emmas\\OneDrive\\Documents\\GitHub\\Housing-and-Migrants-Chicago\\outside_data"
-
 
 data20 <- read_csv(file.path(path, "overdose counts 2020.csv"))
 data21 <- read_csv(file.path(path, "overdose counts 2021.csv"))
