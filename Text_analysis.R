4637fd7e10261902f51fadd58efd384ad24bd451
sgarner796 <sgarner796@gmail.com>
2024-02-28 10:31:11 -0600

    text shiny

--- Text_analysis.R
+++ Text_analysis.R
@@ -5,6 +5,7 @@ library(sentimentr)
 library(udpipe)
 library(SnowballC)
 library(rvest)
+library(stargazer)
 
 setwd("M:/p/GitHub/r_2/Housing-and-Migrants-Chicago/text files texas/full data")
 
@@ -53,7 +54,7 @@ texas_feelings <- texas_feelings %>%
 
 overall_texas_feelings <- texas_feelings %>%
   mutate(article_id = as.numeric(article_id)) %>% 
-  group_by(sentence_id) %>%
+  group_by(article_id) %>%
   summarise(mean_afinn = mean(afinn, na.rm=TRUE),
             median_affin = median(afinn, na.rm=TRUE),
             sd_afinn = sd(afinn, na.rm = TRUE),
@@ -61,7 +62,7 @@ overall_texas_feelings <- texas_feelings %>%
             max_afinn = max(afinn, na.rm = TRUE)) 
 
 overall_texas_feelings %>%
-  ggplot(aes(x = desc(sentence_id), y = mean_afinn)) +
+  ggplot(aes(x = desc(article_id), y = mean_afinn)) +
   geom_line(color = "#00BFC4", size = 1) +  # Line for sentiment scores
   geom_point(color = "#F8766D", size = 2, alpha = 0.8) +  # Points for individual data
   geom_smooth(method = "lm", color = "blue", se = TRUE, linetype = "dashed") + 
@@ -83,7 +84,6 @@ Operation_lonestar_df <- Operation_lonestar_df %>%
   mutate(article_id = 1:103)
 
 
-
 dates <- Operation_lonestar_df %>% 
   select(article_id,full_date)
 
@@ -94,6 +94,10 @@ chicago_newcomers <- read.csv("Southern_Border_Arrivals_to_Chicago_-_2022-Presen
 chicago_newcomers <-chicago_newcomers %>% 
   mutate(Date = mdy(Date))
 
+chicago_newcomers_reg <-chicago_newcomers %>% 
+  mutate(month_year = paste(year(Date), month(Date), sep = "-"))
+
+
 texas_sent <- left_join(texas_sent,dates, by = "article_id")
 
 texas_sent <- texas_sent %>%
@@ -103,9 +107,78 @@ texas_sent <- texas_sent %>%
   select(full_date,Count, article_id,City,) %>% 
   mutate(Count = as.numeric(Count))
 
+### Graph From abbots news
+
+
+texas_graph <- texas_sent %>%
+  mutate(month_year = paste(year(full_date), month(full_date), sep = "-")) %>%
+  arrange(month_year) %>% 
+  group_by(month_year,City) %>%
+  summarise(Count = sum(Count, na.rm = TRUE))
+
+texas_graph <- texas_graph %>% 
+  mutate(per_cent = if_else(row_number() == 1, 0, (Count / lag(Count) - 1) * 100))
+
+##texas graph more digested
+
+texas_graph <- texas_graph %>%
+  mutate(month_year = as.Date(paste0(month_year, "-01"), format = "%Y-%m-%d"))
+
+ggplot(texas_graph, aes(x = month_year, y = log(Count), color = City)) +
+  geom_line() + # Draw lines
+  geom_point() + # Add points to make individual counts more visible
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y")+
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + # Improve x-axis label readability
+  labs(x = "Time", y = "Count", title = "Count Over Time by City")
+
+chicago_sent <- texas_sent %>%
+  filter(City == 'Chicago') %>%
+  mutate(month_year = paste(year(full_date), month(full_date), sep = "-"))
  
+ggplot(texas_graph, aes(x = month_year, y = log(Count), group = City, color = City)) +
+  geom_line(aes(alpha = (City != "Chicago"))) +  
+  geom_point(aes(alpha = (City != "Chicago"))) +  
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y")+
+  scale_color_manual(values = c("Chicago" = "red", 
+                                "Other" = "grey")) +  
+  scale_alpha_manual(values = c(1, 0.3)) + 
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),  
+        legend.position = "none") +  # Move legend to bottom
+  labs(x = "Time", y = "Count", title = "Count Over Time vs Chicago", color = "City", alpha = NULL)  
 
-regresion_df <- left_join(texas_sent, overall_texas_feelings,by = "article_id" )
+regresion_df <- left_join(chicago_sent, overall_texas_feelings,by = "article_id" )
 
-regresion_df <- left_join(chicago_newcomers,regresion_df, by = c( "Date" = "full_date" ))
+regresion_df <- regresion_df %>%
+  arrange(month_year) %>% 
+  group_by(month_year) %>%
+  summarise(Count = sum(Count, na.rm = TRUE),
+            afinn = mean(mean_afinn, na.rm = TRUE))
+
+regresion_df <- regresion_df %>% 
+  mutate(per_cent = if_else(row_number() == 1, 0, (Count / lag(Count) - 1) * 100))
+
+
+regresion_chicago_df <- left_join(chicago_newcomers_reg,regresion_df, by = 'month_year')
+regresion_chicago_df <- regresion_chicago_df %>% 
+  drop_na()
+
+  
+reg_newcomers <- lm(Cumulative.Total ~ Count + afinn + per_cent + Date  , data = regresion_chicago_df )
+
+regresion_chicago_df$lag_Cumulative_Total <- lag(regresion_chicago_df$Cumulative.Total, 1)
+
+reg_newcomers_2 <- lm(Cumulative.Total ~ Count + afinn + per_cent + Date + lag_Cumulative_Total , data = regresion_chicago_df )
+
+summary(reg_newcomers_2)
+
+
+
+# Create the stargazer table with both regression models
+stargazer(reg_newcomers, reg_newcomers_2, type = "text", 
+          title = "Regression Results", no.space = TRUE, 
+          single.row = TRUE, digits = 2, 
+          intercept.bottom = FALSE, omit.stat = c("LL", "ser", "f", "adj.rsq"))
+
          
---
+++ text_shiny.R
@@ -0,0 +1,102 @@
+library(shiny)
+library(ggplot2)
+library(plotly)
+
+setwd("M:/p/GitHub/r_2/Housing-and-Migrants-Chicago/outside_data")
+
+texas_feelings <- read.csv("texas_feelings.csv")
+texas_graph<- read.csv("texas_graph.csv")
+
+#UI
+ui <- fluidPage(
+  # Application title
+  titlePanel("Texas Sentiment and Count Analysis"),
+  
+  # Create tabs
+  tabsetPanel(
+    tabPanel("Sentiment Analysis", 
+             selectInput("statType", "Choose Statistic:", 
+                         choices = c("Mean" = "mean_afinn", 
+                                     "Median" = "median_affin", 
+                                     "Max" = "max_afinn",
+                                     "Min" = "min_afinn",
+                                     "SD"= "sd_afinn")),
+             plotlyOutput("sentimentPlot")),
+    tabPanel("Count Over Time", 
+             selectInput("citySelect", "Choose a city:", 
+                         choices = unique(texas_graph$City), 
+                         selected = "Chicago", multiple = TRUE),
+             plotlyOutput("countPlot"))
+  )
+)
+
+# SERVER
+server <- function(input, output) {
+  
+  # For Sentiment Analysis Plot
+  output$sentimentPlot <- renderPlotly({
+    req(input$statType)  # Ensure that statType is not NULL or missing
+    
+    overall_texas_feelings <- texas_feelings %>%
+      mutate(article_id = as.numeric(article_id)) %>% 
+      group_by(article_id) %>%
+      summarise(mean_afinn = mean(afinn, na.rm=TRUE),
+                median_affin = median(afinn, na.rm=TRUE),
+                sd_afinn = sd(afinn, na.rm = TRUE),
+                min_afinn = min(afinn, na.rm = TRUE),
+                max_afinn = max(afinn, na.rm = TRUE)) 
+    
+    # Dynamically select the statistic based on user input
+    sentiment_stat <- overall_texas_feelings %>% 
+      select(article_id, !!sym(input$statType)) %>%
+      arrange(desc(article_id))  
+    
+    # Convert ggplot to plotly for interactivity
+    p <- ggplot(sentiment_stat, aes(x = article_id, y = !!sym(input$statType))) +
+      geom_line(color = "#00BFC4", size = 1) +  
+      geom_point(color = "#F8766D", size = 2, alpha = 0.8) +  
+      theme_minimal() +  
+      labs(
+        title = "Overall Sentiment  by  Article across time",
+        subtitle = "Sentiment scores with linear regression line",
+        x = "Article ID",
+        y = "Mean Sentiment Score"
+      ) +
+      theme(
+        plot.title = element_text(face = "bold", hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5),
+        legend.position = "none",
+        axis.text.x = element_text(angle = 45, hjust = 1),
+      )  
+    
+    ggplotly(p)
+  })
+      
+
+  # For Count Over Time Plot
+  output$countPlot <- renderPlotly({
+    
+    # Validate input
+    validate(
+      need(input$citySelect, "Please select at least one city.")
+    )
+    
+    filtered_data <- texas_graph %>%
+      filter(City %in% input$citySelect)
+  
+    
+    p <- ggplot(filtered_data, aes(x = month_year, y = log(Count), color = City)) +
+      geom_line() + 
+      geom_point() + 
+      scale_x_date(date_breaks = "1 year", date_labels = "%Y")+
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+      labs(x = "Time", y = "Count", title = "Count Over Time by City") 
+    
+    ggplotly(p)
+   
+  })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
