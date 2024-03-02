7489455b9a8591740c0618f56d5066e8b37003cf
eshultgren <129089900+eshultgren@users.noreply.github.com>
2024-03-02 15:37:25 -0600

    added opioids

--- staticplot.R
+++ staticplot.R
@@ -425,3 +425,35 @@ ggsave("veteran_pop_plot.png", plot = veteran_pop_plot, width = 10, height = 6, 
 
 # Export Combined Dashboard
 ggsave("combined_dashboard.png", plot = combined_dashboard, width = 20, height = 12, units = "in", dpi = 300)
+
+
+
+#### Opioid Static Plot ####
+
+##This plot represented overdose rate by zip code with high concentrations of unhoused folks by year. In order to get 2021 and 2022, update plot name, fill variable, and title
+
+opioid_rate_2020 <- ggplot() +
+  geom_sf(data = zip_chi_shape) +
+  geom_sf(data = opioid_shape, aes(fill = overdose_rate_20), color = NA) +
+  labs(title = "Opioid Overdose Rate (Nonfatal and Fatal Per 10,000 Ppl) by Zip in 2020",
+       subtitle = "Dot Size Correlated W/ High-Density Unhoused Pop.",
+       caption = "Data: IL Dept of Public Health, ACS, PIT Survey") +
+  scale_fill_viridis(name="Opioid Overdose Rate", option = "magma", trans = "reverse", 
+                     breaks = pretty_breaks(n = 5)) +
+  geom_point(data = unhoused_locations, aes(x = Longitude, y = Latitude,
+                                            size = Responses,),
+             color = "#8c62aa",alpha = 0.7) +
+  guides(size = FALSE) +
+  theme_map() +
+  theme(
+    plot.title = element_text(hjust = 0, size= 13),
+    plot.caption = element_text(face = "italic", size = 8),
+    plot.subtitle = element_text(size = 10, hjust = 0),
+    legend.title = element_text(size = 12),
+    legend.title.align = 0.5
+  )
+
+print(opioid_rate_2020)
+
+## 2021 is similiar but uses fill = overdose_rate_21
+#same with 2022 where fill = overdose_rate_22
